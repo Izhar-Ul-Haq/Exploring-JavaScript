cec31e7a259917a3da26d68afe1782b4eae7576b
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-21 20:32:05 +0500

    JavaScript Folder

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+console.time('your code time starts now!!!');
+console.log("Izhar Khan Khattak");
+console.log("Izhar I am a Computer Science Engineering Student");
+console.log(2*3); // 6
+console.log(2*3*4); // 24
+console.log(2+3-4); // 1
+console.log(2+3+4); // 9
+console.log(true);
+console.log(false);
+console.log([1,2,3,4,5,6]);
+console.log({izhar:"Izhar Ul Haq", ulHaq:"Ul Haq", marks:99});
+console.table({izhar:"Izhar Ul Haq", ulHaq:"Ul Haq", marks:99});
+console.warn("This is warning baby!!!");
+// console.clear();
+console.timeEnd('Your code took this much time brother!!!');
+let age = 500;
+console.assert(age<200, 'Not possible my friend');
+console.error("Hi i am error");
+
